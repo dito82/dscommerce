f30904037d4b38813a9b0b6daa5522b3559a8d49
Expedito Filho <exp.filho.junior@gmail.com>
2024-02-17 15:51:31 -0300

    Exception handling

---
+++ src/main/java/com/devsuperior/dscommerce/services/excepetions/DataBaseException.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dscommerce.services.excepetions;
+
+public class DataBaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
